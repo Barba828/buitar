88972c1d4f763d2baddc0bbe417df6131eb2ee69
Wayland Zhan <routerhex@qq.com>
2024-05-06 02:50:23 +0000

    feat: docker build with only nginx proxy version

--- dockerbuil.sh
+++
@@ -1,3 +0,0 @@
-# Default build script
-# Run Example: docker run --rm -p 8282:8282 jalr4ever/buitar
-docker build -t jalr4ever/buitar:latest -f dockerfile/Dockerfile.pnpm .
--- dockerbuild.nginx.sh
+++
@@ -1,3 +0,0 @@
-# Nginx version build script
-# Run Example: docker run --rm -p 8282:8282 jalr4ever/buitar
-docker build -t jalr4ever/buitar:latest -f dockerfile/Dockerfile.nginx .
